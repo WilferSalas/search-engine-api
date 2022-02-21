543d25fe5c8f1754e26b18c3edfe668abe99d2ae
Wilfer Salas <sony634@hotmail.com>
2022-02-20 22:02:31 -0500

    refactor: remove unnecessary route and service

--- routes/movies.routes.ts
+++ routes/movies.routes.ts
@@ -27,14 +27,4 @@ router.get('/movie/:id', (req, res) => {
   return res.status(404).send('Movie not found');
 });
 
-router.post('/similar', (req, res) => {
-  const { body: { searchTerm } } = req;
-
-  const movieTitles = service.findOneSimilar(searchTerm);
-
-  if (movieTitles) return res.status(201).json(movieTitles);
-
-  return res.status(404).send('Error getting movie titles');
-});
-
 export default router;
--- services/movies.service.ts
+++ services/movies.service.ts
@@ -24,11 +24,6 @@ class MoviesService {
   findOne(id) {
     return this.movies.find(movie => movie.id === id);
   };
-
-  findOneSimilar(serachTerm) {
-    return this.movies.filter((item) =>
-      item.title.toLocaleLowerCase().includes(serachTerm?.toLocaleLowerCase())).slice(0, 4);
-  };
 };
 
 export default MoviesService;
